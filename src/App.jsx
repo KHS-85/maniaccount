919081b8845d3d1f03ce494a52419c70fe63f00a
KHS-85 <60042770+KHS-85@users.noreply.github.com>
2021-08-11 15:41:18 +0200

    season 8
    Updated for season 8 and added footer text

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,14 @@ function App() {
 
       <img src={ManiacDino} alt="" />
 
-      <h1>Get ready for season 7!</h1> <br /><br />
+      <h1>Get ready for season 8!</h1> <br /><br />
 
       <span id="redtext">
         <h2>Servers will open in:</h2>
 
         <h1>
 
-          <Countdown date='Jul 9 2021 19:00:00 GMT+0100'>
+          <Countdown date='Aug 13 2021 19:00:00 GMT+0100'>
             <Completionist />
           </Countdown>
 
@@ -35,6 +35,14 @@ function App() {
 
 
       </span>
+
+      <footer>
+        <div className="trent">
+          <h6>Made with
+            <span className="heart"> &#9829; </span>
+            by Trent Steel</h6>
+        </div>
+      </footer>
 
 
     </div>
